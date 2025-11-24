4dd6576d0d95e0396f52372a30f72da3ab123c3b
Michael Julius <mjulius161@gmail.com>
2025-11-24 21:50:10 +0100

    school pyhon OOP 2c

---
+++ My-code/2c.py
@@ -0,0 +1,139 @@
+planets = {
+    "Mercurius": ("maan1", "maan2"),
+    "Venus": ("maan1", "maan2"),
+    "Aarde": ("maan1", "maan2"),
+    "Mars": ("maan1", "maan2"),
+    "Jupiter": ("maan1", "maan2"),
+    "Saturnus": ("maan1", "maan2"),
+    "Uranus": ("maan1", "maan2"),
+    "Neptunu": ("maan1", "maan2")
+}
+
+class Fleet(object):
+    def __init__(self, locations) -> None:
+        self.ships = []
+        self.locations = locations
+    
+    def add_ship(self, ship) -> None:
+        self.ships.append(ship)
+        print(f"ship: {ship.name} added to fleet")
+        print(f"Fleet now has: {[ship.name for ship in self.ships]}")
+        
+    def send_ship(self, ship, planet, moon=None) -> None:
+        if ship.current_location == ship.destination or ship.current_location is None:
+
+            if planet in self.locations:
+
+                if moon is None:
+                    ship.destination = planet
+                    print(f"ship: {ship.name} is flying to: {ship.destination}")
+                elif moon in self.locations[planet]:
+                    ship.destination = (planet, moon)
+                    print(f"ship: {ship.name} is flying to: {moon} van {planet}")
+                else:
+                    print(f"{moon} is not a valid moon of {planet}")
+            
+            else:
+                print("vul een geldige planeet in ")
+                print(f"planeten: {list(self.locations.keys())}")
+
+    def ship_arrived(self, ship) -> None:
+        ship.current_location = ship.destination
+        print(f"ship: {ship.name} arrived at: {ship.destination}")
+
+
+    def status(self):
+        fleet = self.ships
+        print(f"fleet status:")
+
+        for ship in fleet:   
+            print(f"  {ship.name}: current_location={ship.current_location}, destination={ship.destination}")
+
+    def send_free_ship(self, planet, moon=None) -> None:
+        for ship in self.ships:
+            if ship.destination == None:
+
+                if planet in self.locations:
+
+                    if moon is None:
+                        ship.destination = planet
+                        print(f"ship: {ship.name} is flying to: {ship.destination}")
+                        break
+                    elif moon in self.locations[planet]:
+                        ship.destination = (planet, moon)
+                        print(f"ship: {ship.name} is flying to: {moon} van {planet}")
+                        break
+                    else:
+                        print(f"{moon} is not a valid moon of {planet}")
+                
+                else:
+                    print("vul een geldige planeet in ")
+                    print(f"planeten: {list(self.locations.keys())}")
+
+    def check_locations(self):
+        return self.locations
+
+class Spaceship(object):
+    def __init__(self, name):
+        self.name = name 
+        self.current_location = None
+        self.destination = None
+
+
+    def print(self) -> str:
+        return self.name
+
+
+if __name__ == '__main__':
+    s = Spaceship('SS-Aplha')
+    print(s.print())   
+    v = Spaceship('SS-Vanguard')
+    print(v.print()) 
+    e = Spaceship('SS-Explorer')
+    print(e.print()) 
+    a = Spaceship('SS-Aurora')
+    print(a.print()) 
+    o = Spaceship('SS-Obsidian')
+    print(o.print()) 
+    n = Spaceship('SS-Nova')
+    print(n.print()) 
+    p = Spaceship('SS-Phoenix')
+    print(p.print()) 
+    r = Spaceship('SS-Orion')
+    print(r.print()) 
+
+    print("-" * 40)
+
+    f = Fleet(planets)
+
+    f.add_ship(s)
+    f.add_ship(v)
+    f.add_ship(e)
+    f.add_ship(a)
+    f.add_ship(o)
+    f.add_ship(n)
+    f.add_ship(p)
+    f.add_ship(r)
+
+    print("-" * 40)
+
+    f.send_ship(s,'Mercurius')
+    f.send_ship(v,'Venus', 'maan1')
+    print()
+    f.send_free_ship('Uranus')
+    f.send_free_ship('Neptunu', 'maan2')
+    
+    print("-" * 40) 
+
+    f.ship_arrived(s)
+
+    print("-" * 40) 
+
+    f.status()
+
+
+
+    
+   
+
+
