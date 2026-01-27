f131f0b1176c2c8c770760dda04b522ef4f45e6b
Michael Julius <mjulius161@gmail.com>
2026-01-27 13:21:22 +0100

    project

--- Project-Timaflu/backend/inkoop_proces_use.py
+++ Project-Timaflu/backend/inkoop_proces_use.py
@@ -2,12 +2,14 @@ from inkoop_proces import Product, Product_Leverancier, Inkoop_Proces, Voorraad_
 
 def main():
 
+    # Dummy producten
     p1 = Product(1, "Paracetamol", 2.55, 100, 250, Voorraad_status.OP_VOORRAAD)
     p2 = Product(2, "Ibuprofen", 3.10, 80, 60, Voorraad_status.ONDER_MINIMUM)
     p3 = Product(3, "Amoxicilline", 12.99, 50, 0, Voorraad_status.NIET_OP_VOORRAAD)
     p4 = Product(4, "Aspirine", 2.80, 120, 130, Voorraad_status.OP_VOORRAAD)
     p5 = Product(5, "Insuline", 45.00, 30, 10, Voorraad_status.ONDER_MINIMUM)
 
+    # Dummy leveranciers
     pl1 = Product_Leverancier(p1, "PharmaSupply BV", 1.80, Leverbaarheid_Status.LEVERBAAR)
     pl2 = Product_Leverancier(p5, "MedicoGroothandel", 5.40, Leverbaarheid_Status.LEVERBAAR)
     pl3 = Product_Leverancier(p2, "HealthCore NV", 2.10, Leverbaarheid_Status.NIET_LEVERBAAR)
@@ -22,13 +24,10 @@ def main():
     producten = [p1,p2,p3,p4,p5]
     leveranciers = [pl1,pl2,pl3,pl4,pl5,pl6,pl7,pl8,pl9,pl10]
 
+    # Inkoopproces starten
     inkoop = Inkoop_Proces(producten, leveranciers) 
-
-
-  
     inkoop.controleer_voorraad()
     inkoop.start_inkopen()
-
 if __name__ == '__main__':
     main()
 
